daffdb8a975e5dacd93bb14902eb7cd642e296af
Adam Jackson <akjackson1@gmail.com>
2016-02-25 09:25:21 -0500

    Limit SDABC for load more

--- application/controllers/egw.php
+++ application/controllers/egw.php
@@ -9,12 +9,18 @@ class Egw extends CI_Controller
 		$this->load->helper( "url" );
 		$this->load->model( "egwmodel" );
 	}
+	
 	function get()
 	{
+		$filter = "AND active = 1";
+		if( FULL ) { //TEMPORARY ACCESS TO EGW
+			$filter = "";
+		}
+		
 		$ref = $this->uri->segment( 3 );
 		$offset = $this->uri->segment( 4 ) ? $this->uri->segment( 4 ) : 0;
 		
-		$data = json_encode( $this->egwmodel->verse_references( $ref, 10, $offset ) );
+		$data = json_encode( $this->egwmodel->verse_references( $ref, 10, $offset, $filter ) );
 		$this->output->set_output( $data );
 	}
 
