1e8f9277384acc9fe73d2db6d08db8dfd1941931
Adam Jackson <akjackson1@gmail.com>
2018-08-04 07:38:54 -0700

    Fix JS bug

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 if( ENVIRONMENT == "production" ) {
-	$config['base_url'] = "http://bibletools.info";
+	$config['base_url'] = "https://bibletools.info";
 } elseif( ENVIRONMENT == "testing" ) {
 	$config['base_url'] = "http://beta.bibletools.info";
 } else {
--- application/views/template.php
+++ application/views/template.php
@@ -20,7 +20,7 @@
 	<link href='https://fonts.googleapis.com/css?family=Lato:300,400,300italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	<script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<?php if( ENVIRONMENT == "production" ) { ?>
 		<link href="/assets/app.min.css?v=2.0" rel="stylesheet">
 		<script async type="text/javascript" src="/assets/app.min.js?v=2.1"></script>
